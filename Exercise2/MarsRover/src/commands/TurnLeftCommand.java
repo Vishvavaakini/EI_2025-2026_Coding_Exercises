6d437b37c262fe9f49b09b11e574c721d3911ad4
Vishvavaakinii <vish0vaks@gmail.com>
2025-10-02 09:55:35 +0530

    feat(command): add logging for TurnLeftCommand execution

--- Exercise2/MarsRover/src/commands/TurnLeftCommand.java
+++ Exercise2/MarsRover/src/commands/TurnLeftCommand.java
@@ -1,10 +1,18 @@
 package src.commands;
 
 import src.core.Rover;
+import src.utils.Logger;
 
 public class TurnLeftCommand implements Command {
+
     @Override
     public void execute(Rover rover) {
         rover.turnLeft();
+        Logger.logCommandWithPosition(getName(), rover.getStatus().position(), rover.getStatus().direction());
+    }
+
+    @Override
+    public String getName() {
+        return "Turn Left";
     }
 }
