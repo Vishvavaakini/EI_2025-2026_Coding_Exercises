3b4eeb2b8015bb9ad23b313de0ce434cb5447e48
Vishvavaakinii <vish0vaks@gmail.com>
2025-10-02 09:56:17 +0530

    feat(command): add logging for TurnRightCommand execution

--- Exercise2/MarsRover/src/commands/TurnRightCommand.java
+++ Exercise2/MarsRover/src/commands/TurnRightCommand.java
@@ -1,10 +1,18 @@
 package src.commands;
 
 import src.core.Rover;
+import src.utils.Logger;
 
 public class TurnRightCommand implements Command {
+
     @Override
     public void execute(Rover rover) {
         rover.turnRight();
+        Logger.logCommandWithPosition(getName(), rover.getStatus().position(), rover.getStatus().direction());
+    }
+
+    @Override
+    public String getName() {
+        return "Turn Right";
     }
 }
