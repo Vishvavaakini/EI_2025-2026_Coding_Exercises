5d46c1a6ffa7e69b553f284ea24d7d5b4fc9faaa
Vishvavaakinii <vish0vaks@gmail.com>
2025-10-02 09:54:59 +0530

    feat(command): add logging for MoveCommand execution and obstacle detection

--- Exercise2/MarsRover/src/commands/MoveCommand.java
+++ Exercise2/MarsRover/src/commands/MoveCommand.java
@@ -2,10 +2,19 @@ package src.commands;
 
 import src.core.Rover;
 import src.exception.OutOfBoundException;
+import src.utils.Logger;
 
 public class MoveCommand implements Command {
+
     @Override
     public void execute(Rover rover) throws OutOfBoundException {
         rover.move();
+        Logger.logCommandWithPosition(getName(), rover.getStatus().position(), rover.getStatus().direction());
     }
+
+    @Override
+    public String getName() {
+        return "Move";
+    }
+
 }
